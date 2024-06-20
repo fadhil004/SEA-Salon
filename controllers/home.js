3a16d9fd8af12dfafd6e9d6c323f1d1d10e7401f
fadhil004 <m.fadhil238@gmail.com>
2024-06-20 14:57:13 +0700

    fetch review from database

---
+++ controllers/home.js
@@ -0,0 +1,19 @@
+const { Review } = require('../models');
+
+class HomeController{
+    static async index(req, res) {
+        try {
+            // Mengambil 5 review terakhir berdasarkan ID
+            const reviews = await Review.findAll({
+                order: [['id', 'DESC']], // Mengurutkan berdasarkan ID secara menurun
+                limit: 5 // Mengambil hanya 5 data terakhir
+            });
+            res.render('index', { reviews });
+        } catch (err) {
+            res.render('error', { error: err.message });
+        }
+    }
+    
+}
+
+module.exports = HomeController;
--- routers/index.js
+++ routers/index.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 const ReservationController = require('../controllers/reservation');
 const ReviewController = require('../controllers/review');
+const HomeController = require('../controllers/home');
 
+router.get('/', HomeController.index)
 
-
-router.get('/', (req,res) => {
-    res.render('index')
-})
 router.get('/reservation', (req,res) => {
     res.render('reservation')
 })
-
 router.post('/reservation/add', ReservationController.create)
 
 router.post('/review', ReviewController.create)
